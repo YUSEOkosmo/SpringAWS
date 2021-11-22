7f74004f3cc722b872c3d9c3c7a5d948ad6ea7cf
YUSEO <hs671005@naver.com>
2021-11-22 17:31:28 +0900

    스프링 AWS 6장부터 다시

--- src/main/java/com/demo/Spring/boot/Application.java
+++ src/main/java/com/demo/Spring/boot/Application.java
@@ -2,6 +2,7 @@ package com.demo.Spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
--- src/main/java/com/demo/Spring/boot/controller/dto/IndexController.java
+++ src/main/java/com/demo/Spring/boot/controller/dto/IndexController.java
@@ -1,5 +1,7 @@
 package com.demo.Spring.boot.controller.dto;
 
+import com.demo.Spring.boot.config.auth.LoginUser;
+import com.demo.Spring.boot.config.auth.dto.SessionUser;
 import com.demo.Spring.boot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,16 +10,25 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService service;
+    //private final HttpSession session;
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser sUser){
         model.addAttribute("posts",service.findAllDesc()); 
         //findAllDesc를 통해서 불러온 data를 posts라는 이름으로 index에 맵핑
 
+        //SessionUser sUser = (SessionUser) session.getAttribute("user");
+
+        if(sUser != null){
+            model.addAttribute("userName",sUser);
+            System.out.println("NAME: " + sUser.getName() + "EMAIL: " + sUser.getEmail());
+        }
         return "index";
     }
 
