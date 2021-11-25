ce771f2eab50ca658aeddb566efa68d7992965f6
YUSEO <hs671005@naver.com>
2021-11-25 11:07:13 +0900

    9.5 배포자동화

---
+++ scripts/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=SpringAWS
+echo ">빌드파일 복사"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+echo ">구동중인 어플 PID확인"
+CURRENT_PID=$(pgrep -fl SpringAWS | grep jar | awk '{print $1}')
+echo "> PID: $CURRENT_PID"
+if [ -z "$CURRENT_PID" ]; then
+  echo "> 구동중인게없음"
+else
+  echo "> KILL -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> 새 어플 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+echo "> JAR NAME: $JAR_NAME"
+echo "> $JAR_NAME 에 권한추가"
+chmod +x $JAR_NAME
+echo "> $JAR_NAME 실행"
+nohup java -jar \
+  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+  -Dspring.profiles.active=real \
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
